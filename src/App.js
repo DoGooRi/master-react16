85c88841b5473696eb9ce5eefcdb8c1f6a618f18
정현호 <jhh@ancle.kr>
2018-04-17 13:40:05 +0900

    Return Types String and Fragments

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+
+
+class RetrunTypes_Fragment extends Component {
+  render() {
+    return (
+      <Fragment>
+        <header></header>
+        <div></div>
+        <footer></footer>
+      </Fragment>
+    )
+  }
+}
+
+class RetrunTypes_String extends Component {
+  render() {
+    return "hello";
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Fragment>
+        <RetrunTypes_Fragment />
+        <RetrunTypes_String />
+      </Fragment>
     );
   }
 }
